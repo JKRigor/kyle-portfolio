0982a7ba29f84ce751f6ad67e76a47206715cf8e
Kyle <kylerigor.1661@gmail.com>
2024-07-31 14:29:10 +0800

    Add ToDo Project

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,5 +1,6 @@
 import styles from './ProjectsStyles.module.css'
 import tl from '../../assets/Telebet.png'
+import td from '../../assets/Todo.png'
 import ProjectCard from '../../common/ProjectCard'
 function Projects() {
   return (
@@ -10,6 +11,12 @@ function Projects() {
             link='https://github.com/JKRigor/telebet'
             h3="Telebet"
             p="Online Gadgets Store"/>
+        </div>
+        <div className={styles.projectsContainer}>
+            <ProjectCard src={td} 
+            link='https://github.com/JKRigor/ToDo'
+            h3="ToDo List"
+            p="Todo List App"/>
         </div>
     </section>
   )
